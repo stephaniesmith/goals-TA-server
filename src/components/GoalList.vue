76dc7c6a1964e70ff69d89d020adf8813d052703
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-10 14:20:26 -0700

    wiring up the put goals

--- src/components/GoalList.vue
+++ src/components/GoalList.vue
@@ -5,17 +5,25 @@
       <Goal v-for="goal in goals"
         :key="goal.id"
         :goal="goal"
+        :onUpdate="handlePut"
       />
     </ul>
   </div>
 </template>
 
 <script>
+import { putGoal } from '../services/api';
+
 import Goal from './Goal';
 export default {
   props: ['goals'],
   components: {
     Goal
+  },
+  methods: {
+    handlePut(goal) {
+      putGoal(goal);
+    }
   }
 };
 </script>
